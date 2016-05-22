374dade1ecb4acb5f38667e87036dde192b452ec
srinivas931 <srinivasreddypakanati93@gmail.com>
2016-05-22 15:50:25 -0400

    Added a for loop code

--- DemoConsoleApp/DemoConsoleApp/Program.cs
+++ DemoConsoleApp/DemoConsoleApp/Program.cs
@@ -15,6 +15,12 @@ namespace DemoConsoleApp
             Console.WriteLine("Another line added by the client");
             
             Console.WriteLine("There you go! There is another modification");
+
+            for(int i=1; i<=10; i++)
+            {
+                int k = 5 * i;
+                Console.WriteLine(k);
+            }
            
         }
     }
