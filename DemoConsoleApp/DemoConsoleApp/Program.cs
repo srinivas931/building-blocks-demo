113e6a21d91ddde1c1160be3be23c9a042aee70e
srinivas931 <srinivasreddypakanati93@gmail.com>
2016-05-22 15:19:02 -0400

    Update Program.cs

--- DemoConsoleApp/DemoConsoleApp/Program.cs
+++ DemoConsoleApp/DemoConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace DemoConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! -  changed on the server");
         }
     }
 }
